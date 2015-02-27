14c2943619fdf3438967c2e75503625ab6c5756c
jcl <joe@lemura.com.au>
2015-02-27 13:08:48 +1100

    first cut
    needs ui work

---
+++ week1/week1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  week1
+//
+//  Created by Joe Lemura on 27/02/2015.
+//  Copyright (c) 2015 Lemura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBAction func unwindFromSegue(segue: UIStoryboardSegue) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
