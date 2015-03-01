d2d2866686345bfdf9be952d2c32b1adfde6fb14
jcl <joe@lemura.com.au>
2015-03-01 17:54:31 +1100

    first cut

---
+++ Calc/Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by Joe Lemura on 1/03/2015.
+//  Copyright (c) 2015 Lemura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
