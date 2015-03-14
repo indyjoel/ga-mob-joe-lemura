5e6a314369aa096e8be6fbf2c4867c01a6fe6003
indyjoel <indyjcl@IndyJCLs-MacBook-Pro.local>
2015-03-14 11:35:37 +1100

    before todo5 (the monster one)

--- Week 3/03/Lesson03/ViewController.swift
+++ Week 3/03/Lesson03/ViewController.swift
@@ -35,11 +35,21 @@ class ViewController: UIViewController {
     // I hope that's right
     
     
-    TODO three: Add and hook up a ‘dismiss’ button below the above mentioned image view that will dismiss the modal dialog. Do this in CODE.
-    
     
     TODO four: Hook up the button on the home screen to push ArrayTableViewController into view (via the navigation controller) when tapped. Do this by triggering a segue from this view controller. The method you are looking for is performSegueWithIdentifier. Find the identifier from the storyboard.
     */
+    @IBAction func todoFour(sender: AnyObject) {
+        
+        // make sure the segue name is right. 
+        // show the main storyboard, select the segue, then goto the properties attributes inspector segue option
+        // that had me stumped, thank goodness for Mr Google!!!
+        
+        performSegueWithIdentifier("show" ,  sender: self)
+        
+        
+    }
+    
+    
 
 }
 
