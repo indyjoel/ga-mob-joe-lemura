0169a49270841907daeaf77205b28f004491c9a6
indyjoel <indyjcl@IndyJCLs-MacBook-Pro.local>
2015-03-14 11:55:06 +1100

    done todo5

--- Week 3/03/Lesson03/ArrayTableViewController.swift
+++ Week 3/03/Lesson03/ArrayTableViewController.swift
@@ -13,7 +13,22 @@ class ArrayTableViewController: UITableViewController {
     /*
         TODO five: Create a subclass of the Animal class, add an instance of that object to be displayed in the table view, then change the displayed string of the subclass to your liking. Your subclass should override the function 'prettyAnimalName' (i.e. you should only need to modify the below array, not the table view code)
     */
-    let animalArray = [Animal(name: "Rover"), Animal(name: "Mittens"), Animal(name: "Ein"), Animal(name: "Sadie")]
+    //need to comment this out as we want to change it below
+    //let animalArray = [Animal(name: "Rover"), Animal(name: "Mittens"), Animal(name: "Ein"), Animal(name: "Sadie")]
+    
+    
+    //created a new subclass of Animal
+    //changed the displayed string from Animal Name to SuperVillian Name
+    class subclassAnimal : Animal {
+        override func prettyAnimalName() -> String {
+            return "SuperVillian name: " + self.name
+        }
+    }
+    
+    //create an instance
+    //let the code do everything else
+    let animalArray = [subclassAnimal(name: "Rover"), subclassAnimal(name: "Mittens"), subclassAnimal(name: "Ein"), subclassAnimal(name: "Sadie")]
+    
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
@@ -31,3 +46,5 @@ class ArrayTableViewController: UITableViewController {
         return cell
     }
 }
+
+
